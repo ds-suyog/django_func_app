942def16b88f8ebfdd24fa62315e383ce215d37d
ds-suyog <suyog.sethia@gmail.com>
2019-06-02 01:01:43 +0530

    updated input form

--- myproject/funcs/templates/input_form.py
+++ myproject/funcs/templates/input_form.py
@@ -1,8 +1,7 @@
 from django import forms
-from django.utils.safestring import mark_safe
 
 class InputForm(forms.Form):
-    factorial = forms.CharField(label='factorial of number', max_length=100)
-    fibonacci = forms.CharField(label='fibonacci of n numbers', max_length=100)
-    armstrong = forms.CharField(label='is number armstrong?', max_length=100)
+    factorial = forms.CharField(label='factorial of number', max_length=3)
+    fibonacci = forms.CharField(label='fibonacci of n numbers', max_length=3)
+    armstrong = forms.CharField(label='is number armstrong?', max_length=10)
     palindrome = forms.CharField(label='is input palindrome?', max_length=100)
